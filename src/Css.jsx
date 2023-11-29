60719e4df586ecde3164e1c7f206b7cfd76fad98
Jerem16 <jeremy.lemaignent@gmail.com>
2023-11-29 17:39:54 +0100

    test spec CSS2

--- src/Css.jsx
+++ src/Css.jsx
@@ -1,6 +1,6 @@
 // CssComponent.js
 
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 
 const CssComponent = () => {
     useEffect(() => {
@@ -13,18 +13,22 @@ const CssComponent = () => {
             linkLow.setAttribute("importance", "low");
             document.head.appendChild(linkLow);
         } else if (screenWidth >= 1081 && screenWidth <= 1440) {
+
             const linkLow = document.createElement("link");
             linkLow.rel = "stylesheet";
             linkLow.href = "./1440p-1080p-no-critical.css";
             linkLow.setAttribute("importance", "low");
             document.head.appendChild(linkLow);
-        } else if (screenWidth >= 676 && screenWidth <= 1080) {
+        } else if (screenWidth >= 621 && screenWidth <= 1080) {
+
+
             const linkLow = document.createElement("link");
             linkLow.rel = "stylesheet";
             linkLow.href = "./1080p-675p-no-critical.css";
             linkLow.setAttribute("importance", "low");
             document.head.appendChild(linkLow);
         } else if (screenWidth >= 300 && screenWidth <= 620) {
+
             const linkLow = document.createElement("link");
             linkLow.rel = "stylesheet";
             linkLow.href = "./675p-320p-no-critical.css";
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Css from "./Css";
+import CssComponent from "./Css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <Css />
+            <CssComponent />
             <App />
         </Provider>
     </React.StrictMode>
