e64a5e3212ea8232e73b286f9b4544fc217c021b
Jerem16 <jeremy.lemaignent@gmail.com>
2023-11-29 18:14:11 +0100

    test spec CSS5

--- src/Css.jsx
+++ src/Css.jsx
@@ -1,3 +1,15 @@
+// VotreComposant.js
+
+
+
+// import f4k from "./1440p-1080p.css"
+import f4knc from "./4k-1440p-no-critical.css"
+// import f1440k from "./1440p-1080p.css"
+import f1440knc from "./1440p-1080p-no-Critical.css"
+// import f1080k from "./1080p-675p.css"
+import f1080knc from "./1080p-675p-no-Critical.css"
+// import f320k from "./675p-320p.css"
+import f320knc from "./675p-320p-no-Critical.css"
 import React, { useEffect } from "react";
 
 const CssComponent = () => {
@@ -27,7 +39,7 @@ const CssComponent = () => {
         } else if (screenWidth >= 300 && screenWidth <= 620) {
             const linkLow = document.createElement("link");
             linkLow.rel = "stylesheet";
-            linkLow.href = `${publicUrl}/675p-320p-no-critical.css`;
+            linkLow.href = `${f320knc}`;
             linkLow.setAttribute("fetchpriority", "low");
             document.head.appendChild(linkLow);
         }
