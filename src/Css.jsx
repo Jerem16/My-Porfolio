0dbbf7ced5ae9a7173d4793d0d5f5748f394d175
Jerem16 <jeremy.lemaignent@gmail.com>
2023-11-29 17:29:00 +0100

    test spec CSS1

---
+++ src/Css.jsx
@@ -0,0 +1,39 @@
+// CssComponent.js
+
+import React, { useEffect } from "react";
+
+const CssComponent = () => {
+    useEffect(() => {
+        const screenWidth = window.innerWidth;
+
+        if (screenWidth >= 1441) {
+            const linkLow = document.createElement("link");
+            linkLow.rel = "stylesheet";
+            linkLow.href = "./4k-1440p-no-critical.css";
+            linkLow.setAttribute("importance", "low");
+            document.head.appendChild(linkLow);
+        } else if (screenWidth >= 1081 && screenWidth <= 1440) {
+            const linkLow = document.createElement("link");
+            linkLow.rel = "stylesheet";
+            linkLow.href = "./1440p-1080p-no-critical.css";
+            linkLow.setAttribute("importance", "low");
+            document.head.appendChild(linkLow);
+        } else if (screenWidth >= 676 && screenWidth <= 1080) {
+            const linkLow = document.createElement("link");
+            linkLow.rel = "stylesheet";
+            linkLow.href = "./1080p-675p-no-critical.css";
+            linkLow.setAttribute("importance", "low");
+            document.head.appendChild(linkLow);
+        } else if (screenWidth >= 300 && screenWidth <= 620) {
+            const linkLow = document.createElement("link");
+            linkLow.rel = "stylesheet";
+            linkLow.href = "./675p-320p-no-critical.css";
+            linkLow.setAttribute("importance", "low");
+            document.head.appendChild(linkLow);
+        }
+    }, []);
+
+    return <></>;
+};
+
+export default CssComponent;
---
+++ src/ResponsiveStylesheet.jsx
@@ -0,0 +1,17 @@
+// import React from "react";
+
+// const ResponsiveStylesheet = (props) => {
+//     const { media, href, importance, fetchpriority } = props;
+
+//     return (
+//         <link
+//             rel="stylesheet"
+//             media={media}
+//             href={href}
+//             importance={importance}
+//             fetchpriority={fetchpriority}
+//         />
+//     );
+// };
+
+// export default ResponsiveStylesheet;
