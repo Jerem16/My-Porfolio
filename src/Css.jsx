e6eb6c384586630c2a7f2c491121754759add558
Jerem16 <jeremy.lemaignent@gmail.com>
2023-11-29 17:44:51 +0100

    test spec CSS3

--- src/Css.jsx
+++ src/Css.jsx
@@ -1,6 +1,6 @@
 // CssComponent.js
 
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
 const CssComponent = () => {
     useEffect(() => {
@@ -10,29 +10,26 @@ const CssComponent = () => {
             const linkLow = document.createElement("link");
             linkLow.rel = "stylesheet";
             linkLow.href = "./4k-1440p-no-critical.css";
-            linkLow.setAttribute("importance", "low");
+
+            linkLow.setAttribute("fetchpriority", "low");
             document.head.appendChild(linkLow);
         } else if (screenWidth >= 1081 && screenWidth <= 1440) {
-
             const linkLow = document.createElement("link");
             linkLow.rel = "stylesheet";
             linkLow.href = "./1440p-1080p-no-critical.css";
-            linkLow.setAttribute("importance", "low");
+            linkLow.setAttribute("fetchpriority", "low");
             document.head.appendChild(linkLow);
         } else if (screenWidth >= 621 && screenWidth <= 1080) {
-
-
             const linkLow = document.createElement("link");
             linkLow.rel = "stylesheet";
             linkLow.href = "./1080p-675p-no-critical.css";
-            linkLow.setAttribute("importance", "low");
+            linkLow.setAttribute("fetchpriority", "low");
             document.head.appendChild(linkLow);
         } else if (screenWidth >= 300 && screenWidth <= 620) {
-
             const linkLow = document.createElement("link");
             linkLow.rel = "stylesheet";
-            linkLow.href = "./675p-320p-no-critical.css";
-            linkLow.setAttribute("importance", "low");
+            linkLow.href = `${process.env.PUBLIC_URL}675p-320p-no-critical.css`;
+            linkLow.setAttribute("fetchpriority", "low");
             document.head.appendChild(linkLow);
         }
     }, []);
