2c97e547d9dd51f2bc71e1a1945f05ab641654d0
Jerem16 <jeremy.lemaignent@gmail.com>
2023-11-30 12:17:08 +0100

    test imageRE2

--- src/components/01-Home/MyName.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import Jeremy from "../99-Svg_Icon/jeremy";
-const MyName = () => {
-    return <span className="name"><Jeremy /></span>;
-};
-
-export default React.memo(MyName);
