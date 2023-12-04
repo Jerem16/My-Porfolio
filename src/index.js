0624c3d6e30df28bd810b009840db3e4186563d0
Jerem16 <jeremy.lemaignent@gmail.com>
2023-12-04 17:04:11 +0100

    type MIME CSS dbg5

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-// import CssComponent from "./Css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
@@ -9,7 +8,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            {/* <CssComponent /> */}
             <App />
         </Provider>
     </React.StrictMode>
