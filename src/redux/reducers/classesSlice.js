9bfaec816b423bb83463af0c598d43e7805885dc
Jerem16 <jeremy.lemaignent@gmail.com>
2023-12-16 15:53:14 +0100

    Scroll dbg2-1

--- src/redux/reducers/classesSlice.js
+++ src/redux/reducers/classesSlice.js
@@ -7,7 +7,7 @@ const initialState = {
     // desktop1280: "1280",
     tab1080: "1080",
     menuSwitcher: "langClose",
-    navScroll: "top",
+    navScroll: "home",
 };
 
 const classesSlice = createSlice({
