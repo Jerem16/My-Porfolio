86cc1362aac41f3e791179587d7cacb3992c6ea0
Jerem16 <jeremy.lemaignent@gmail.com>
2024-01-07 21:38:42 +0100

    V162

--- src/components/01-Home/TypedText.jsx
+++ src/components/01-Home/TypedText.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const TypedText = ({ data, typedRef }) => {
     const [windowWidth, setWindowWidth] = useState(window.innerWidth);
