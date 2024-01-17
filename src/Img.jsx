ddab447df45c1c91ed4885f2f9f5912b51f73d3b
Jerem16 <jeremy.lemaignent@gmail.com>
2024-01-17 16:34:04 +0100

    Test SEOptimisationHTML and ImageCOmp

--- src/Img.jsx
+++ src/Img.jsx
@@ -1,24 +1,36 @@
 import React, { useEffect } from "react";
+
 const ImgComponent = () => {
     useEffect(() => {
         const screenWidth = window.innerWidth;
 
         if (screenWidth >= 300 && screenWidth <= 620) {
             import("./assets/img/profile-620.webp").then((module) => {
-                applyStyles(module);
+                applyStyles(module.default);
+            });
+        } else if (screenWidth > 620 && screenWidth <= 1440) {
+            import("./assets/img/profile-1440.webp").then((module) => {
+                applyStyles(module.default);
+            });
+        } else if (screenWidth > 1440 && screenWidth <= 1920) {
+            import("./assets/img/profile-1k.webp").then((module) => {
+                applyStyles(module.default);
+            });
+        } else if (screenWidth > 1920) {
+            import("./assets/img/profile-4k.webp").then((module) => {
+                applyStyles(module.default);
             });
         }
     }, []);
 
-    const applyStyles = (module) => {
-        const linkLow = document.createElement("link");
-        linkLow.rel = "preload";
-        linkLow.type = "image/webp";
-        linkLow.itemprop = "url";
-        linkLow.as = "image";
-        linkLow.href = module.default;
-        linkLow.setAttribute("fetchpriority", "hight");
-        document.head.appendChild(linkLow);
+    const applyStyles = (imageUrl) => {
+        const link = document.createElement("link");
+        link.rel = "preload";
+        link.type = "image/webp";
+        link.as = "image";
+        link.href = imageUrl;
+        link.setAttribute("fetchpriority", "high");
+        document.head.appendChild(link);
     };
 
     return <></>;
