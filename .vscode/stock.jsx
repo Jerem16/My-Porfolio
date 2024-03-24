8b0ede6a6682ab76d672570d636d2d21ffbec3aa
Jerem16 <jeremy.lemaignent@gmail.com>
2024-03-24 20:23:03 +0100

    LinkCSS module.test3

--- .vscode/stock.jsx
+++ .vscode/stock.jsx
@@ -1,51 +1,47 @@
 import { useEffect, useRef } from "react";
 import { useWindowWidth } from "./utils/hooks";
+import css320p from "./assets/css/675p-320p-no-Critical.css";
+import css1080p from "./assets/css/1080p-675p-no-Critical.css";
+import css1440p from "./assets/css/1440p-1080p-no-Critical.css";
+import css4Kp from "./assets/css/4k-1440p-no-critical.css";
 
 const CssLowComponent = () => {
     const windowWidth = useWindowWidth();
-    const cssLink = useRef(null);
+    // const cssLink = useRef(null);
     const mediaLink = useRef(null);
 
     useEffect(() => {
         const screenWidth = window.innerWidth;
-        let newCssLink = "";
+        // let newCssLink = "";
         let newMediaLink = "";
 
         switch (true) {
             case screenWidth >= 300 && screenWidth <= 620:
-                import("./assets/css/675p-320p-no-Critical.css").then(
-                    (module) => {
-                        applyStyles(module.default);
-                    }
-                );
+                import(css320p).then((module) => {
+                    applyStyles(module);
+                });
                 newMediaLink = "(min-width:300px) and (max-width:620px)";
                 // newCssLink = "./assets/css/675p-320p-no-Critical.css";
                 break;
             case screenWidth >= 621 && screenWidth <= 1080:
-                import("./assets/css/1080p-675p-no-Critical.css").then(
-                    (module) => {
-                        applyStyles(module.default);
-                    }
-                );
+                import(css1080p).then((module) => {
+                    applyStyles(module);
+                });
                 newMediaLink = "(min-width:621px) and (max-width:1080px)";
 
                 // newCssLink = "./assets/css/1080p-675p-no-Critical.css";
                 break;
             case screenWidth >= 1081 && screenWidth <= 1440:
-                import("./assets/css/1440p-1080p-no-Critical.css").then(
-                    (module) => {
-                        applyStyles(module.default);
-                    }
-                );
+                import(css1440p).then((module) => {
+                    applyStyles(module);
+                });
                 newMediaLink = "(min-width:1081px) and (max-width:1440px)";
                 // newCssLink = "./assets/css/1440p-1080p-no-Critical.css";
                 break;
             default:
-                import("./assets/css/4k-1440p-no-critical.css").then(
-                    (module) => {
-                        applyStyles(module.default);
-                    }
-                );
+                import(css4Kp).then((module) => {
+                    applyStyles(module);
+                });
                 newMediaLink = "(min-width:1440px)";
             // newCssLink = "./assets/css/4k-1440p-no-Critical.css";
         }
