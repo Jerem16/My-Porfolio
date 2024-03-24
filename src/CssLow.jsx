1a33dd02a22769733b3ef1fb2b208b3ebb8e9d6c
Jerem16 <jeremy.lemaignent@gmail.com>
2024-03-24 21:02:49 +0100

    LinkCSS module.test12

--- src/CssLow.jsx
+++ src/CssLow.jsx
@@ -1,43 +1,56 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import { useWindowWidth } from "./utils/hooks";
+// import css320p from "./assets/css/320pNC.css";
+// import css1080p from "./assets/css/1080pNC.css";
+// import css1440p from "./assets/css/1440pNC.css";
+// import css4Kp from "./assets/css/4kNC.css";
 
 const CssLowComponent = () => {
     const windowWidth = useWindowWidth();
-    const cssLink = useRef(null);
     const mediaLink = useRef(null);
 
     useEffect(() => {
         const screenWidth = window.innerWidth;
+        let newMediaLink = "";
+
         switch (true) {
             case screenWidth >= 300 && screenWidth <= 620:
-                mediaLink.current = "(min-width:300px) and (max-width:620px)";
-                cssLink.current = "./675p-320p-no-critical.css";
+                import("./assets/css/320pNC.css").then((module) => {
+                    applyStyles(module.default);
+                });
+                newMediaLink = "(min-width:300px) and (max-width:620px)";
                 break;
             case screenWidth >= 621 && screenWidth <= 1080:
-                mediaLink.current = "(min-width:621px) and (max-width:1080px)";
-                cssLink.current = "./1080p-675p-no-critical.css";
+                import("./assets/css/1080pNC.css").then((module) => {
+                    applyStyles(module.default);
+                });
+                newMediaLink = "(min-width:621px) and (max-width:1080px)";
                 break;
             case screenWidth >= 1081 && screenWidth <= 1440:
-                mediaLink.current = "(min-width:1081px) and (max-width:1440px)";
-                cssLink.current = "./1440p-1080p-no-critical.css";
+                import("./assets/css/1440pNC.css").then((module) => {
+                    applyStyles(module.default);
+                });
+                newMediaLink = "(min-width:1081px) and (max-width:1440px)";
                 break;
             default:
-                mediaLink.current = "(min-width:1440px)";
-                cssLink.current = "./4k-1440p-no-critical.css";
+                import("./assets/css/4kNC.css").then((module) => {
+                    applyStyles(module.default);
+                });
+                newMediaLink = "(min-width:1440px)";
         }
-
-        addCssLink(cssLink.current, mediaLink.current);
+        mediaLink.current = newMediaLink;
     }, [windowWidth]);
 
-    const addCssLink = (cssLink, mediaLink) => {
-        const existingLink = document.getElementById(`cssLink${cssLink}`);
+    const applyStyles = (module) => {
+        const existingLink = document.getElementById(`cssLink${module}`);
 
         if (!existingLink) {
             const link = document.createElement("link");
             link.rel = "stylesheet";
-            link.media = mediaLink;
-            link.href = cssLink;
-            link.id = `cssLink${cssLink}`;
+            link.media = mediaLink.current; // Utiliser mediaLink.current au lieu de mediaLink
+            link.type = "text/css";
+            link.href = module;
+            link.id = `cssLink${module}`;
             link.setAttribute("fetchpriority", "low");
             document.head.appendChild(link);
         }
